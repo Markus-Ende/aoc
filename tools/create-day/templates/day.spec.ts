e71357943e63509295040655cdda5072c7a74191
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2024-12-02 08:29:34 +0100

    chore: improve spec template

--- tools/create-day/templates/day.spec.ts
+++ tools/create-day/templates/day.spec.ts
@@ -4,19 +4,27 @@ import { part1, part2 } from './day{{day}}';
 describe('day{{day}}', () => {
   test.each`
     input                            | expected
-    ${'{{year}}-day{{day}}'}         | ${undefined}
     ${'{{year}}-day{{day}}-example'} | ${undefined}
-  `('part1 $input $expected', ({ input, expected }) => {
+  `('part1 example $input $expected', ({ input, expected }) => {
     const result = part1(readInput(input));
     expect(result).toEqual(expected);
   });
 
+  test('part1', () => {
+    const result = part1(readInput('{{year}}-day{{day}}'));
+    expect(result).toEqual(undefined);
+  });
+
   test.each`
     input                            | expected
-    ${'{{year}}-day{{day}}'}         | ${undefined}
     ${'{{year}}-day{{day}}-example'} | ${undefined}
-  `('part2 $input $expected', ({ input, expected }) => {
+  `('part2 example $input $expected', ({ input, expected }) => {
     const result = part2(readInput(input));
     expect(result).toEqual(expected);
+  });
+
+  test('part2', () => {
+    const result = part2(readInput('{{year}}-day{{day}}'));
+    expect(result).toEqual(undefined);
   });
 });
